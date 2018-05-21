2063e3f5eb44503ea9e1ce28d9d29c0ef51f7319
Trevor Caudill <trevr.300@gmail.com>
2018-05-21 19:11:46 -0400

    Adding base files

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * GeneratePress child theme functions and definitions.
+ *
+ * Add your custom PHP in this file. 
+ * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ */
+
+function generatepress_child_enqueue_scripts() {
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
